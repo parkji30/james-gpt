9ea927ff7a2d583e1e770ac85713f267d9cc2977
James Park <james.park@xanadu.ai>
2026-03-29 19:32:46 +0000

    Transformer Block

--- model.py
+++ model.py
@@ -3,11 +3,12 @@ from torch import nn
 import torch.nn.functional as F
 
 def softmax(x):
-    x = x - x.max()
+    x = x - x.max() #this makes largest value -> 0 and exp(0) = 1.
     exps = torch.exp(x)
-    return exps / torch.sum(x)
+    return exps / torch.sum(exps)
 
-class SelfAttention(nn.Module):
+
+class CausalSelfAttention(nn.Module):
 
     def __init__(self, in_dim, embedding_dim):
         
@@ -20,13 +21,25 @@ class SelfAttention(nn.Module):
         self.dim_k = embedding_dim ** 0.5
 
     def forward(self, x):
-
+        # Assume shape is (B, L, C)
         q = self.q_embed(x)
         k = self.k_embed(x)
         v = self.v_embed(x)
         
-        self_attention = F.softmax((q @ k.transpose(-2, -1)) / self.dim_k, dim=-1) @ v
-        return self_attention
+        # fill fake scores first
+        scores = ((q @ k.transpose(-2, -1)) / self.dim_k)
+        
+        # Create lower triangular attention
+        mask = torch.tril(
+            torch.ones(scores.size(-2), scores.size(-1), device=scores.device, dtype=torch.bool)
+        )
+
+        # Fill the forbidden spots with -inf.
+        scores = scores.masked_fill(~mask, float("-inf"))
+
+        attention_scores = torch.vmap(softmax)(scores) @ v
+
+        return attention_scores
 
 
 class MLP(nn.Module):
@@ -58,11 +71,25 @@ class MLP(nn.Module):
             
 class TransformerBlock(nn.Module):
 
-    def __init__(self):
+    def __init__(self, embed_dim:int = 512, mlp_dim:int=2048):
         super().__init__()
 
+        self.mlp = MLP(embed_dim, mlp_dim)
+
+        self.causal_attention = CausalSelfAttention()
+
+        self.ln1 = nn.LayerNorm(embed_dim)
+        self.ln2 = nn.LayerNorm(embed_dim)
+
     def forward(self, x):
-        ...
+
+        # Causal Attention Block
+        x = x + self.causal_attention((self.ln1(x)))
+
+        # FF Block
+        x = x + self.mlp(self.ln2(x))
+
+        return x
 
 
 class GPT(nn.Module):
